581aad9fb71cfcc0ace1e7ac9d22e109841a1178
Aaron Morton <santasarmy@gmail.com>
2013-11-18 00:35:59 -0500

    Working Feed-Forward
    Implemented working feed-forward mechanism, and changed structure of
    layer Array. It is now a list of arrays, with each array corresponding
    to a layer, and the ith element in that array corresponding to the bias
    of that particular node. Right now it is initialized to 0

--- artificialNeuralNetwork.py
+++ artificialNeuralNetwork.py
@@ -16,7 +16,19 @@ class ANN():
             layer 0 is the input layer, and the last layer is the output layer
             Weights are randomly generated b/w 0 and 1
         '''
-        self.layerArray = hiddenLayers
+        
+        #create layerArray matrix, initialized with biases of 0 for each node
+        #the biases for the input layer should always be 0 anyway (I think)
+        hiddenLayersWeights = []
+        for i in range(len(hiddenLayers)):
+            layer = []
+            for j in range(hiddenLayers[i]):
+                layer.append(0)
+            hiddenLayersWeights.append(layer)
+        self.layerArray = hiddenLayersWeights
+        
+        #create list of weight matrices, with random weights for each connection
+        #one weight matrix for each connection b/w layers (e.g) one less than number of layers
         matrixList = []
         for i in range(len(hiddenLayers)-1):
             weightMatrix = numpy.matrix(numpy.random.rand(hiddenLayers[i+1],hiddenLayers[i]))
@@ -25,6 +37,9 @@ class ANN():
         
     def getWeightMatrixList(self):
         return self.weightMatrixList
+    
+    def getHiddenLayers(self):
+        return self.layerArray
     
     def feed(self,inputList):
         '''
@@ -37,26 +52,29 @@ class ANN():
         
         'main external loop, pass along activation at each layer, and pass it along'
         for i in range(len(self.weightMatrixList)):
-            weightMatrix = self.weightMatrixList(i)
-            newInput = []
+            weightMatrix = self.weightMatrixList[i]
+            accumInput = []
             'main internal loop, calculate weighted activation sum at each node in current layer'
-            for j in range(self.layerArray[i+1]):
-                acc = 0
+            for j in range(len(self.layerArray[i+1])):
+                #set the accumulator to initially be the bias of this node
+                acc = (self.layerArray[i+1])[j]
                 for k in range(len(input)):
-                    acc += weightMatrix(j,k)*input(k)
-                    
-                newInput[j]=acc
-                
-            'FILL IN REST HERE, CHECK CORRECTNESS OF ABOVE'
-            input = newInput
+                    acc += weightMatrix[j,k]*input[k]
+                accumInput.append(acc)
+            input = accumInput
+            
+        return input
             
             
     
 def main():
-    testAnn = ANN([5,10,33,1])
-    weights = testAnn.getWeightMatrixList()
-    for mat in weights:
-        print(mat)
-        print(numpy.shape(mat))
+    testAnn = ANN([5,30,12,1])
+#     weights = testAnn.getWeightMatrixList()
+#     for mat in weights:
+#         print(mat)
+#         print(numpy.shape(mat))
+#     print(testAnn.getHiddenLayers())
+    print(testAnn.feed([2,1,1,2,1]))
+    print(testAnn.feed([2,1,1,2,1]))    
     
 main()
